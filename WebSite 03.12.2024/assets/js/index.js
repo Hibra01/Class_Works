9fb2552a2064c67307646f0ab9ccd07e0c0ad336
Hibra01 <mydevgml@gmail.com>
2024-12-03 12:14:55 +0400

    edit

--- WebSite 03.12.2024/assets/js/index.js
+++ WebSite 03.12.2024/assets/js/index.js
@@ -1,19 +1,18 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menuMobile = document.querySelector(".menu-desk");
+const a = document.querySelectorAll(".a");
 
 let isMobile = false;
-
 menuBtn.addEventListener("click", function(){
-    // menuMobile.classList.closest("show-menu");
-    console.log("Hello");
-    if(isMobile == false){
-        console.log("Yes");
-        isMobile = true;
-        menuMobile.classList.add("show-menu");
-    }
-    else if(isMobile == true){
-        console.log("Not");
-        isMobile = false;
-        menuMobile.classList.remove("show-menu");
-    }
+    menuMobile.classList.toggle("show-menu");
+    a.forEach((tag) => tag.classList.toggle("hide-a"));
+
+    // if(isMobile == false){
+    //     isMobile = true;
+    //     menuMobile.classList.add("show-menu");
+    // }
+    // else if(isMobile == true){
+    //     isMobile = false;
+    //     menuMobile.classList.remove("show-menu");
+    // }
 });
