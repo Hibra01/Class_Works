4fdfe5ee93cd286f15053226dc029a2631622dcb
Hibra01 <mydevgml@gmail.com>
2024-12-16 23:17:34 +0400

    test

--- index.js
+++ index.js
@@ -70,7 +70,7 @@
 
 
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const array = [0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10];
 // 26
 
 // array.forEach((a,i) => a%2!=0 && console.log(i))
@@ -94,3 +94,35 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // 31
 
 // console.log(array.reduce((min, num) => (num < min ? num : min), array[0]))
+
+// 32
+
+// console.log(array.filter((a) => a%2 == 0 && a != 0).reduce((max, num) => (num > max ? num : max), array.filter((a) => a%2 == 0 && a != 0)[0]))
+
+// 33
+
+// console.log(array.filter((a) => a%2 == 0 && a != 0).reduce((min, num) => (num < min ? num : min), array.filter((a) => a%2 == 0 && a != 0)[0]))
+
+// 34
+
+// console.log(array.filter((a) => a%2 != 0 && a != 0).reduce((max, num) => (num > max ? num : max), array.filter((a) => a%2 == 0 && a != 0)[0]))
+
+// 35
+
+// console.log(array.filter((a) => a%2 != 0 && a != 0).reduce((min, num) => (num < min ? num : min), array.filter((a) => a%2 == 0 && a != 0)[0]))
+
+// 36
+
+// console.log(array.findIndex((a) => a == array.reduce((max, num) => (num > max ? num : max), array[0])))
+
+// 37
+
+// console.log(array.findIndex((a) => a == array.reduce((min, num) => (num < min ? num : min), array[0])))
+
+// 38
+
+// console.log(array.reduce((count, num) => num == array.reduce((max, num) => (num > max ? num : max), array[0]) && ++count, 0))
+
+// 39
+
+// console.log(array.reduce((count, num) => num == array.reduce((min, num) => (num < min ? num : min), array[0]) && ++count, 0))
