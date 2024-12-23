d7e840d423f7bf9ed4f93bf0321c590c986f7705
Hibra01 <mydevgml@gmail.com>
2024-12-23 13:45:11 +0400

    tasks

--- FindGame/game.js
+++ FindGame/game.js
@@ -4,16 +4,17 @@ let array = []
 
 cards.forEach((a) =>
     a.addEventListener("click", function () {
-        if (array.length < 2) {
-            a.children[0].classList.toggle("show")
-            a.children[1].classList.toggle("hide")
-            array.push(a.getAttribute("data-id"))
-            if (array.length == 2) {
-                // console.log(compareCards(array))
-                compareCards(array)
-            }
-            // console.log(array)
+        if (array.length >= 2) {
+            return
         }
+        a.children[0].classList.replace("hide", "show")
+        a.children[1].classList.replace("show", "hide")
+        array.push(a.getAttribute("data-id"))
+        if (array.length == 2) {
+            // console.log(compareCards(array))
+            compareCards(array)
+        }
+        // console.log(array)
     })
 )
 
@@ -22,11 +23,24 @@ function compareCards(array) {
     // console.log(array[1])
     if (array[0] == array[1]) {
         cards.forEach((a) => {
+            console.log(a)
             if (a.getAttribute("data-id") == array[0]) {
-                console.log(a)
-                a.children[0].classList.toggle("hide")
-                a.children[1].classList.toggle("hide")
-                // a.children[2].classList.toggle("show")
+                console.log(array)
+                a.children[0].classList.replace("show", "hide")
+                a.children[1].classList.replace("show", "hide")
+                a.children[2].classList.replace("hide", "show")
+            }
+        }
+        )
+    }
+    else {
+        cards.forEach((a) => {
+            console.log(a)
+            if (a.getAttribute("data-id") == array[0]) {
+                console.log("Not")
+                a.children[0].classList.replace("show", "hide")
+                a.children[1].classList.replace("hide", "show")
+                a.children[2].classList.replace("show", "hide")
             }
         }
         )
---
+++ React Apps/react_useEffect/src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [products, setProducts] = useState([])
+  const [inp, setInp] = useState([])
+
+
+  function getData() {
+    fetch("https://676925b9cbf3d7cefd39c232.mockapi.io/api/fakevatsap")
+    .then((res) => res.json())
+    .then((data) => setProducts(data))
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+  
+  return (
+    <>
+      <form onSubmit={(e) => setProducts()}>
+        <div className="mb-3">
+          <input type="text" value={inp} className="form-control" id="exampleInputPassword1" onChange={(e) => setInp(e.target.value)}/>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+
+      <ul>{products.map((a) => <li key={a.id}>{a.message}</li>)}</ul>
+    </>
+  )
+}
+
+export default App
